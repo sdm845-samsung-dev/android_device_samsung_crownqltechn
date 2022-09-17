sm8250-samsung/android_device_samsung_y2q

Initial skeleton device tree

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_y2q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_y2q-user \
+    lineage_y2q-userdebug \
+    lineage_y2q-eng

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/y2q

sdm845-samsung-dev/android_device_samsung_crownqltechn

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_crownqlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_crownqlte-user \
+    lineage_crownqlte-userdebug \
+    lineage_crownqlte-eng

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common OEM chipset BoardConfig.
+include device/samsung/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/crownqlte